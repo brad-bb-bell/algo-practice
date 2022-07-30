17fe1c05a9e5ec7d0fb8ec52c85ea9f977dbb960
Brad Bell <brad.bb.bell@gmail.com>
2022-07-30 17:06:50 -0600

    adds largest product

---
+++ trello-ladder/largestproduct.rb
@@ -0,0 +1,29 @@
+# Find the largest product of any two numbers within a given array.
+
+# Input: [5, -2, 1, -9, -7, 2, 6]
+# Output: 63 (-9 * -7)
+
+def largest_product(array)
+  solution_product = array[0] * array[1]
+  index = 0
+  inner = 0
+  while index < array.length
+    while inner < array.length
+      if index == inner
+        inner += 1
+      elsif array[index] * array[inner] > solution_product
+        solution_product = array[index] * array[inner]
+        solution1 = array[index]
+        solution2 = array[inner]
+        inner += 1
+      else
+        inner += 1
+      end
+    end
+    inner = 0
+    index += 1
+  end
+  return "#{solution_product} (#{solution1} * #{solution2})"
+end
+
+p largest_product([5, -2, 1, -9, -7, 2, 6])
