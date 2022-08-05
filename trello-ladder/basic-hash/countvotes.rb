394812f18c9cac4c4d2a64a160f641e6d119eea1
Brad Bell <brad.bb.bell@gmail.com>
2022-08-05 10:39:11 -0600

    adds count votes

---
+++ trello-ladder/basic-hash/countvotes.rb
@@ -0,0 +1,21 @@
+# Given an array of strings, return a hash that provides of a count of how many times each string occurs.
+
+# Input: ["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"]
+
+# Output: {"Dewey" => 6, "Truman" => 5}
+
+# Explanation: "Dewey" occurs 6 times in the array, while "Truman" occurs 5 times.
+
+def count_votes(array)
+  final_tally = {}
+  array.each do |name|
+    if final_tally[name]
+      final_tally[name] += 1
+    else
+      final_tally[name] = 1
+    end
+  end
+  final_tally
+end
+
+p count_votes(["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"])
