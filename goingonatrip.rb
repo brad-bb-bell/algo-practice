76d5b8c3d7c1efc7188441650f59b9a1f1edfcfa
Brad Bell <brad.bb.bell@gmail.com>
2022-08-04 22:21:44 -0600

    going on a trip

---
+++ goingonatrip.rb
@@ -0,0 +1,16 @@
+# Leaving for Granite Peak this morning!
+
+# The tallest peak in Montana.
+
+# I attempted it last year and failed miserably.
+
+# Now I'm taking the lesson that I learned last year and bringing my brother and I think we have a great chance of success.
+
+# Good luck to me! See you in three days :)
+
+
+# Welp, we went.
+
+# We had great success!
+
+# It was so fun and a great experience to share with my brother.
