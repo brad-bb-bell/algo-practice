b10a89b8e413ea1de32eac51ae718eaa2bef27a5
Brad Bell <brad.bb.bell@gmail.com>
2022-08-12 10:09:19 -0600

    adjusts for camelcase in reverse array

--- trello-ladder/basic-array/reversearray.js
+++ trello-ladder/basic-array/reversearray.js
@@ -3,7 +3,7 @@
 // Input: [1, 2, 3, 4, 5]
 // Output: [5, 4, 3, 2, 1]
 
-function reversearray(array) {
+function reverseArray(array) {
   var solution = [];
   for (let index = array.length - 1; index >= 0; index--) {
     solution.push(array[index]);
@@ -11,4 +11,4 @@ function reversearray(array) {
   return solution;
 }
 
-console.log(reversearray([1, 2, 3, 4, 5]));
+console.log(reverseArray([1, 2, 3, 4, 5]));
