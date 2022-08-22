8d917cbc8f967547d4df18dd78f3224f3737f569
Brad Bell <brad.bb.bell@gmail.com>
2022-08-22 14:09:03 -0600

    adds hamming.js

---
+++ trello-ladder/basic-string/hamming.js
@@ -0,0 +1,22 @@
+// Given two strings of equal length, write a function that returns the number of characters that are different between the two strings.
+
+// Input: "ABCDEFG", "ABCXEOG"
+// Output: 2
+
+// Explanation: While the A, B, C, E, and G are in the same place for both strings, they have different characters in the other spaces. Since there are 2 such spaces that are different (the D and F in the first string), we return 2.
+
+// Input: "ABCDEFG", "ABCDEFG",
+// Output: 0
+
+function hamming(string1, string2) {
+  var differences = 0;
+  for (let index = 0; index < string1.length; index++) {
+    if (string1[index] !== string2[index]) {
+      differences += 1;
+    }
+  }
+  return differences;
+}
+
+console.log(hamming("ABCDEFG", "ABCXEOG"));
+console.log(hamming("ABCDEFG", "ABCDEFG"));
